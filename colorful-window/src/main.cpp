7f4fed323b7df6719f3bec5e8ec01510fc82a62b
nihad1213 <nihad.nemetli@gmail.com>
2025-12-11 22:26:05 +0400

    finished: colorful window created

--- colorful-window/src/main.cpp
+++ colorful-window/src/main.cpp
@@ -3,6 +3,34 @@
 
 int main() {
 
-    std::cout << "Test" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize glfw!" << std::endl;
+        return -1; 
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Colorful Screen", NULL, NULL);
+
+    if (!window) {
+        std::cerr << "Failed to create window!" << std::endl; 
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.0f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }

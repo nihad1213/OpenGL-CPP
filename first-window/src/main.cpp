febf58eb5827d295f2f9739225b3fe2fe635bca8
nihad1213 <nihad.nemetli@gmail.com>
2025-12-11 04:09:33 +0400

    new: First window created

--- first-window/src/main.cpp
+++ first-window/src/main.cpp
@@ -1,6 +1,43 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "test";
+
+    if (!glfwInit()) {
+        std::cerr << "Failted to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set OpenGL Version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    // Create window
+    GLFWwindow *window = glfwCreateWindow(800, 600, "First Screen", NULL, NULL);
+
+    // Create window
+    if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Main Loop
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
+
 }

53270f2c0577be86e64d7b175f894c36075b6465
nihad1213 <nihad.nemetli@gmail.com>
2025-12-11 23:20:37 +0400

    new: main.cpp file created

---
+++ 03.first-triangle/src/main.cpp
@@ -0,0 +1,36 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "First Triangle", NULL, NULL);
+
+    if (!window) {
+        std::cerr << "Failed to initialize window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}

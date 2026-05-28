f94c0666b73203508d79c8ad9f8bd4811e9213e3
Nihad <nihad.nemetli@gmail.com>
2026-05-28 20:08:16 +0400

    add: background is ready

--- 04.first-line/src/main.cpp
+++ 04.first-line/src/main.cpp
@@ -3,7 +3,41 @@
 #include <iostream>
 
 int main() {
+    
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize glfw!" << std::endl;
+        return -1; 
+    }
 
-    std::cout << "test";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "First Line", NULL, NULL);
+
+    if (!window) {
+        std::cerr << "Failed to create window!" << std::endl; 
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD!" << std::endl;
+        return -1;
+    }
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.0f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }

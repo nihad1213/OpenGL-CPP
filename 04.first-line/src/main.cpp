f3fea7458ea290a8d1da807d1f2798180a4b042d
Nihad <nihad.nemetli@gmail.com>
2026-05-28 20:15:15 +0400

    new: vertex and fragment shaders added now line shown

--- 04.first-line/src/main.cpp
+++ 04.first-line/src/main.cpp
@@ -2,53 +2,81 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char* vertexShaderSrc = R"(
+#version 330 core
+layout (location = 0) in vec2 aPos;
+
+void main() {
+    gl_Position = vec4(aPos, 0.0, 1.0);
+}
+)";
+
+const char* fragmentShaderSrc = R"(
+#version 330 core
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+)";
+
 int main() {
-    
-    GLfloat vertices[] = {
-        700, 100, 0,
-        900, 100, 0
-    };
-
-
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize glfw!" << std::endl;
-        return -1; 
-    }
+    glfwInit();
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "First Line", NULL, NULL);
-
-    if (!window) {
-        std::cerr << "Failed to create window!" << std::endl; 
-        glfwTerminate();
-        return -1;
-    }
-
+    GLFWwindow* window = glfwCreateWindow(800, 600, "First Line", nullptr, nullptr);
     glfwMakeContextCurrent(window);
-    
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD!" << std::endl;
-        return -1;
-    }
 
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glViewport(0, 0, 800, 600);
+
+    float vertices[] = {
+        -0.5f,  0.0f,
+         0.5f,  0.0f
+    };
+
+    GLuint vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShaderSrc, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShaderSrc, nullptr);
+    glCompileShader(fs);
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glDisableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(3, GL_FLOAT, 0, vertices);
+        glUseProgram(program);
+        glBindVertexArray(vao);
         glDrawArrays(GL_LINES, 0, 2);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
     glfwTerminate();
-
-    return 0;
 }
